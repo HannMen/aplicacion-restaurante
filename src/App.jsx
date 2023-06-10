af5a5da98e72b4e9dc60322b307a634351ae87d0
HannMen <hannermendozasanchez@gmail.com>
2023-06-10 10:38:18 -0400

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import GeneralInfo from "./pages/GeneralInfo";
+import ContactUs from "./pages/ContactUs";
+import Location from "./pages/Location";
+
+const App = () => {
+  return (
+    <>
+      <GeneralInfo />
+      <ContactUs />
+      <Location />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/GeneralInfo.jsx
@@ -0,0 +1,7 @@
+const GeneralInfo = () => {
+  return (
+    <h1>General Info</h1>
+  )
+}; 
+
+export default GeneralInfo
