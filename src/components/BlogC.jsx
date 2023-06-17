eb19b72ea0c2c0a6096647817d2e3b872e9df361
HannMen <hannermendozasanchez@gmail.com>
2023-06-16 22:45:26 -0400

    Sexto  Commit

--- src/components/BlogC.jsx
+++ src/components/BlogC.jsx
@@ -5,7 +5,7 @@ const BlogC = () => {
     <div className="container">
       <h2 className="blog-title text-center">Latest Blog Posts</h2>
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-6">
           <div className="card blog-card">
             <img src="image1.jpg" className="card-img-top" alt="Blog Post 1" />
             <div className="card-body">
@@ -21,7 +21,7 @@ const BlogC = () => {
             </div>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-6">
           <div className="card blog-card">
             <img src="image2.jpg" className="card-img-top" alt="Blog Post 2" />
             <div className="card-body">
@@ -38,7 +38,7 @@ const BlogC = () => {
             </div>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-6">
           <div className="card blog-card">
             <img src="image3.jpg" className="card-img-top" alt="Blog Post 3" />
             <div className="card-body">
@@ -47,6 +47,22 @@ const BlogC = () => {
                 Maecenas ut semper turpis. Suspendisse commodo libero elit, et
                 posuere ex congue sed. Nunc maximus neque vel velit vulputate
                 aliquet. In suscipit tortor eget elementum consectetur.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card blog-card">
+            <img src="image4.jpg" className="card-img-top" alt="Blog Post 4" />
+            <div className="card-body">
+              <h5 className="card-title">Post Title 4</h5>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                aliquet, ligula in feugiat feugiat, odio sapien fringilla
+                sapien, ac sagittis tellus turpis eu elit.
               </p>
               <a href="#" className="btn btn-primary">
                 Read More
